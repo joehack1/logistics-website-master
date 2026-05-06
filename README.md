31e0c2ed4cc0cd8cdcde555faed8b7d68a63d0bd
Joel <joelluke100@gmail.com>
2026-05-06 22:07:14 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# logistics-website
+http://ubuntuclearing.co.ke/
+this is a static HTML Website. Feel free to use
+
---
+++ footer.php
@@ -0,0 +1,54 @@
+<footer>
+        		<div class="color-part2"></div>
+        		<div class="color-part"></div>
+        		<div class="container-fluid">
+            		<div class="row block-content">
+               			<div class="col-sm-4 wow zoomIn" data-wow-delay="0.3s">
+                    		 <img src="img/logosimple.png" width="200px" alt="UBUNTU CLEARING & FORWARDING LIMITED"/>
+	                        <p>UBUNTU CLEARING &amp; FORWARDING LIMITED is a well-established supply chain solutions firm majoring in logistics and consultancy.</p>
+	                        <div class="footer-icons">
+	                            <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+	                            <a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+	                            <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+	                            <a href="#"><i class="fa fa-pinterest-square fa-2x"></i></a>
+	                            <a href="#"><i class="fa fa-vimeo-square fa-2x"></i></a>
+	                        </div>
+	                        <a href="contact.php" class="btn btn-lg btn-danger">CONTACT US TODAY</a>
+	                    </div>
+	                    <div class="col-sm-2 wow zoomIn" data-wow-delay="0.3s">
+                    		<h4>WHAT WE OFFER</h4>
+	                        <nav>
+                        		<a href="clearance.php">Clearance & Forwarding</a>
+	                            <a href="road-trucking.php">Road Transportation</a>
+	                            <a href="air-freight.php">Air Freight</a>
+	                            <a href="goods.php">Import & Exports of Goods</a>
+	                            <a href="warehousing.php">Warehousing</a>
+	                            <a href="supply-chain-management.php">Supply Chain</a>
+	                        </nav>
+	                    </div>
+	                    <div class="col-sm-2 wow zoomIn" data-wow-delay="0.3s">
+                    		<h4>MAIN LINKS</h4>
+	                        <nav>
+                        		<a href="index.php">Home</a>
+	                            <a href="services.php">Our Services</a>
+	                            <a href="about-us.php">About Us</a>
+	                            
+	                            <a href="contact.php">Contact</a>
+	                        </nav>
+	                    </div>
+	                    <div class="col-sm-4 wow zoomIn" data-wow-delay="0.3s">
+                    		<h4>CONTACT INFO</h4>
+	                        Everyday is a new day for us and we work really hard to satisfy our customers everywhere.
+	                        <div class="contact-info">
+	                            <span><i class="fa fa-location-arrow"></i><strong>UBUNTU CLEARING &amp; FORWARDING LIMITED 
+</strong><br>Sifa Towers, Kilimani,Ring Road</span>
+	                            <span><i class="fa fa-phone"></i>Tel: 0745607694/0743457168/0751345293 </span>
+	                            <span><i class="fa fa-envelope"></i>info@Ubuntuclearing.co.ke </span>
+	                            <span><i class="fa fa-clock-o"></i>Mon - Sat  8.00 - 17.00</span>
+	                        </div>
+	                    </div>
+	                </div>
+             		<div class="copy text-right"><a id="to-top" href="#this-is-top"><i class="fa fa-chevron-up"></i></a> <a href="#"></a> &copy; 2026 UBUNTU CLEARING &amp; FORWARDING LIMITED All rights reserved.</div>
+	             </div>
+	        </footer>
+
